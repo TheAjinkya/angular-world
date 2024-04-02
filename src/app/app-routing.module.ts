8d8e3ea57b5ff6eb7eac7a0161692002093948a4
Ajinkya Chanshetty <ajinkya.chanshetty@gmail.com>
2024-04-02 23:26:37 +0100

    Created the forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TodoFormComponent } from './todo-form/todo-form.component';
+import { ReactiveFormComponent } from './form-wizard/reactive-form/reactive-form.component';
 
 const routes: Routes = [{path:'dashboard', component: DashboardComponent},
-{path:'todo', component: TodoFormComponent}];
+{path:'todo', component: TodoFormComponent},
+{path:'registration', component: ReactiveFormComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/form-wizard/reactive-form/reactive-form.component.ts
+++ src/app/form-wizard/reactive-form/reactive-form.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent {
+  registrationForm = new FormGroup({
+    firstName: new FormControl("",[Validators.required, Validators.maxLength(5)]),
+    lastName: new FormControl("",[Validators.required, Validators.minLength(2)]),
+    contact: new FormControl("9990900099",[Validators.required, Validators.maxLength(5)]),
+    email: new FormControl("abc@gmail.com",[Validators.required, Validators.maxLength(5)]),
+  });
 
+  onSubmit(){
+    console.log(this.registrationForm.value);
+  }
 }
